d9916f179703a350e9c5a6ae462f2d1a3fd9fef7
Miacid <jkl@Jeffs-Mac.local>
2025-05-09 19:24:07 -0700

    WIP removing HAL calls from setup.

--- software/src/gamesquirrel/main.c
+++ software/src/gamesquirrel/main.c
@@ -16,15 +16,8 @@ static const uint32_t blink_interval_ms = 250;
 #define BUTTON_PORT           GPIOC
 #define BUTTON_PIN            GPIO_PIN_13
 
-// UART Enable for STLink VCOM
+// UART
 #define UART_DEV              USART3
-#define UART_CLK_EN           __USART3_CLK_ENABLE
-#define UART_GPIO_PORT        GPIOA
-#define UART_GPIO_AF          GPIO_AF13_USART3
-
-#define UART_TX_PIN           GPIO_PIN_3
-#define UART_RX_PIN           GPIO_PIN_4
-
 #define CFG_BOARD_UART_BAUDRATE 115200   ///< Default baud rate
 
 void board_led_write(bool state);
@@ -104,69 +97,88 @@ void hw_init(void)
 }
 
 static inline void SystemClock_Config(void) {
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+	RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+	RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-  /** Configure the main internal regulator output voltage
-  */
-  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE0);
+	// set highest voltage on regulator before setting clocks to high speed
+	PWR->VOSCR = 0x30; //SCALE0, highest voltage
+	while ((PWR->VOSSR & PWR_VOSSR_VOSRDY) == 0)
+	{}
 
-  while(!__HAL_PWR_GET_FLAG(PWR_FLAG_VOSRDY)) {}
+	/** Initializes the RCC Oscillators according to the specified parameters
+	* in the RCC_OscInitTypeDef structure.
+	*/
+	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI48|RCC_OSCILLATORTYPE_HSE;
+	RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+	RCC_OscInitStruct.HSI48State = RCC_HSI48_ON;
+	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+	RCC_OscInitStruct.PLL.PLLSource = RCC_PLL1_SOURCE_HSE;
+	RCC_OscInitStruct.PLL.PLLM = 12;
+	RCC_OscInitStruct.PLL.PLLN = 250;
+	RCC_OscInitStruct.PLL.PLLP = 2;
+	RCC_OscInitStruct.PLL.PLLQ = 2;
+	RCC_OscInitStruct.PLL.PLLR = 2;
+	RCC_OscInitStruct.PLL.PLLRGE = RCC_PLL1_VCIRANGE_1;
+	RCC_OscInitStruct.PLL.PLLVCOSEL = RCC_PLL1_VCORANGE_WIDE;
+	RCC_OscInitStruct.PLL.PLLFRACN = 0;
+	HAL_RCC_OscConfig(&RCC_OscInitStruct);
 
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI48|RCC_OSCILLATORTYPE_HSE;
-  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-  RCC_OscInitStruct.HSI48State = RCC_HSI48_ON;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLL1_SOURCE_HSE;
-  RCC_OscInitStruct.PLL.PLLM = 12;
-  RCC_OscInitStruct.PLL.PLLN = 250;
-  RCC_OscInitStruct.PLL.PLLP = 2;
-  RCC_OscInitStruct.PLL.PLLQ = 2;
-  RCC_OscInitStruct.PLL.PLLR = 2;
-  RCC_OscInitStruct.PLL.PLLRGE = RCC_PLL1_VCIRANGE_1;
-  RCC_OscInitStruct.PLL.PLLVCOSEL = RCC_PLL1_VCORANGE_WIDE;
-  RCC_OscInitStruct.PLL.PLLFRACN = 0;
-  HAL_RCC_OscConfig(&RCC_OscInitStruct);
+	/** Initializes the CPU, AHB and APB buses clocks
+	*/
+	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+	                             |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2
+	                             |RCC_CLOCKTYPE_PCLK3;
+	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+	RCC_ClkInitStruct.APB3CLKDivider = RCC_HCLK_DIV1;
+	HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5);
 
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                                |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2
-                                |RCC_CLOCKTYPE_PCLK3;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
-  RCC_ClkInitStruct.APB3CLKDivider = RCC_HCLK_DIV1;
-  HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5);
 
-  // Configure CRS clock source
-  __HAL_RCC_CRS_CLK_ENABLE();
-  RCC_CRSInitTypeDef RCC_CRSInitStruct = {0};
-  RCC_CRSInitStruct.Prescaler = RCC_CRS_SYNC_DIV1;
-  RCC_CRSInitStruct.Source = RCC_CRS_SYNC_SOURCE_USB;
-  RCC_CRSInitStruct.Polarity = RCC_CRS_SYNC_POLARITY_RISING;
-  RCC_CRSInitStruct.ReloadValue = __HAL_RCC_CRS_RELOADVALUE_CALCULATE(48000000, 1000);
-  RCC_CRSInitStruct.ErrorLimitValue = 34;
-  RCC_CRSInitStruct.HSI48CalibrationValue = 32;
-  HAL_RCCEx_CRSConfig(&RCC_CRSInitStruct);
+	/* Peripheral clock enable */
 
-  /* Select HSI48 as USB clock source */
-  RCC_PeriphCLKInitTypeDef usb_clk = {0 };
-  usb_clk.PeriphClockSelection = RCC_PERIPHCLK_USB;
-  usb_clk.UsbClockSelection = RCC_USBCLKSOURCE_HSI48;
-  HAL_RCCEx_PeriphCLKConfig(&usb_clk);
+	// RCC->AHB1ENR = 0x90000100;
 
-  /* Peripheral clock enable */
-  __HAL_RCC_GPIOA_CLK_ENABLE();
-  __HAL_RCC_GPIOB_CLK_ENABLE();
-  __HAL_RCC_GPIOC_CLK_ENABLE();
-  __HAL_RCC_GPIOD_CLK_ENABLE();
-  __HAL_RCC_GPIOH_CLK_ENABLE();
+	RCC->AHB2ENR =
+		RCC_AHB2ENR_SRAM2EN |
+		// RCC_AHB2ENR_DAC1EN |
+		// RCC_AHB2ENR_ADCEN |
+		RCC_AHB2ENR_GPIOAEN |
+		RCC_AHB2ENR_GPIOBEN |
+		RCC_AHB2ENR_GPIOCEN |
+		RCC_AHB2ENR_GPIODEN |
+		RCC_AHB2ENR_GPIOHEN;
 
+	RCC->APB1LENR =
+		RCC_APB1LENR_CRSEN |
+		RCC_APB1LENR_USART3EN;
+
+	RCC->APB1HENR = 0;
+
+	RCC->APB2ENR =
+		RCC_APB2ENR_USBEN;
+
+	// FIXME USB seems to mostly work without CRS, maybe keep it turned off
+	// since we'll be using a clock derived from HSE and the PLLs for the
+	// final USB config.
+	//
+	// My H503 only adjusts the trim between 0x1F and 0x20
+
+	CRS->CFGR =
+		CRS_CFGR_SYNCSRC_1 | // Sync Source == 2 fo USB
+		(34 << 16) |
+		(47999);
+
+	CRS->CR =
+		CRS_CR_CEN |
+		CRS_CR_AUTOTRIMEN |
+		(32 << 8);
+
+	/* Select HSI48 as USB clock source */
+	RCC->CCIPR4 =
+		(0 << 2) | // SYSTICKSEL == rcc_hclk/8
+		(3 << 4);  // USBSEL == hsi48_ker_clk
 }
 
 void board_init(void)
@@ -374,7 +386,6 @@ void board_init(void)
 		(3 << 0) | // PH0     OSC_IN         OSC_IN
 		(3 << 2);  // PH1     Button2        OSC_OUT
 
-	UART_CLK_EN();
 	UartHandle = (UART_HandleTypeDef) {
 	  .Instance        = UART_DEV,
 	  .Init.BaudRate   = CFG_BOARD_UART_BAUDRATE,
@@ -389,8 +400,6 @@ void board_init(void)
 	HAL_UART_Init(&UartHandle);
 
 	// USB
-	__HAL_RCC_USB_CLK_ENABLE();
-
 	/* Enable VDDUSB */
 #if defined (PWR_USBSCR_USB33DEN)
 	HAL_PWREx_EnableVddUSB();
@@ -431,7 +440,8 @@ int main(void)
 		board_led_write(led_state);
 		led_state = 1 - led_state; // toggle
 
-		printf("interrupts  %d\r\n", UsbInterruptCount());
+		//printf("interrupts  %d\r\n", UsbInterruptCount());
+		printf("CRS CR =  %.8lX CFGR = %.8lX\r\n", CRS->CR, CRS->CFGR);
 	}
 }
 
